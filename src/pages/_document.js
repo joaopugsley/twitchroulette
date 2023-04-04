c70e506402c0b4d84a6449ee1d846fbb1e5ff7fe
Insannityxd <canal.insannity@gmail.com>
2023-04-03 22:24:12 -0300

    Adicionando título, tags, etc.

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,16 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>TwitchRoulette</title>
+        <meta name="description" content="TwitchRoulette is a website that conducts giveaways for Twitch streamers. Make your broadcasts more fun with our giveaway tools."></meta>
+        <meta name="keywords" content="Twitch, TwitchRoulette, giveaway, streamers, tools, broadcasts, roulette, livestreaming"></meta>
+        <meta name="author" content="TwitchRoulette"></meta>
+        <meta property="og:title" content="TwitchRoulette - Giveaways for Twitch Streamers"></meta>
+        <meta property="og:description" content="Make your broadcasts more fun with our giveaway tools."></meta>
+        <meta property="og:url" content="https://twitchroulette.live"></meta>
+        <meta property="og:type" content="website"></meta>
+      </Head>
       <body>
         <Main />
         <NextScript />
--- src/pages/index.js
+++ src/pages/index.js
@@ -118,8 +118,8 @@ export default function Home() {
     <main>
       <div className="bg-purple w-screen h-screen flex justify-center items-center scroll-smooth">
         <div className="absolute right-2 top-2 flex flex-row justify-between w-[90px] select-none">
-          <img onClick={() => {changeLanguage("pt")}} className="w-[40px] cursor-pointer hover:scale-110" src="https://media.discordapp.net/attachments/1085074834523967569/1092597129018617886/image.png"/>
-          <img onClick={() => {changeLanguage("en")}} className="w-[40px] cursor-pointer hover:scale-110" src="https://cdn.discordapp.com/attachments/1085074834523967569/1092597230248144896/image.png"/>
+          <img alt="Português Brasileiro (pt-BR)" onClick={() => {changeLanguage("pt")}} className="w-[40px] cursor-pointer hover:scale-110" src="https://media.discordapp.net/attachments/1085074834523967569/1092597129018617886/image.png"/>
+          <img alt="English (en-US)" onClick={() => {changeLanguage("en")}} className="w-[40px] cursor-pointer hover:scale-110" src="https://cdn.discordapp.com/attachments/1085074834523967569/1092597230248144896/image.png"/>
         </div>
         <div className="p-10 flex flex-col justify-center items-center">
           <h1 className="text-4xl font-extrabold text-white text-center select-none">TwitchRoulette!</h1>
