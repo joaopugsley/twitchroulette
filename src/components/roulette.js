24057e660f72a8ae78510bd9c93665007112e1a6
Insannityxd <canal.insannity@gmail.com>
2023-04-04 11:09:00 -0300

    melhorando ux no mobile e desktop

--- src/components/roulette.js
+++ src/components/roulette.js
@@ -113,6 +113,21 @@ export default function Roulette({ participants = [] }) {
 
             {
                 !rolling && !winner ? (
+                    <svg xmlns="http://www.w3.org/2000/svg" width="38" height="38" viewBox="0 0 38 38" stroke="#6441a5">
+                        <g fill="none" fill-rule="evenodd">
+                            <g transform="translate(1 1)" stroke-width="2">
+                                <circle stroke-opacity=".5" cx="18" cy="18" r="18"/>
+                                <path d="M36 18c0-9.94-8.06-18-18-18">
+                                    <animateTransform attributeName="transform" type="rotate" from="0 18 18" to="360 18 18" dur="1s" repeatCount="indefinite"/>
+                                </path>
+                            </g>
+                        </g>
+                    </svg>
+                ) : null
+            }
+
+            {
+                !rolling && !winner ? (
                     <div onClick={() => {roll()}} className="border border-purple mt-3 pl-[20px] pr-[20px] h-[40px] rounded-xl flex flex-row justify-center items-center text-purple select-none hover:scale-105 transition-all ease-in-out duration-50">{t("roulette_draw_button")}</div>
                 ) : null
             }
